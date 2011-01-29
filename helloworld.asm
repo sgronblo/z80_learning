6076efa2d4f7096bc89cf838b3a3e4b0a17ed3ae
Sam Grönblom <sgronblo@gmail.com>
2011-01-29 22:35:51 +0200

    Added initial version of helloworld.asm

---
+++ helloworld.asm
@@ -0,0 +1,30 @@
+Init32 equ 7bh
+ChPut equ #a2
+ChGet equ 9fh
+BinarySize
+    size 8000h
+StartAddress
+    org 4000h
+
+RomInit
+    db "AB"
+    dw Start
+    dw 0,0,0
+    dw 0,0,0
+
+Start
+    call Init32
+    ld a, (HelloString)
+    call PrintString
+    ret
+
+PrintString
+    cp 0 ; compare current char to NIL
+    jp z PrintStringEnd ; if it was NIL go to end
+    call ChPut ; print the current char
+    inc a ; go to next char
+PrintStringEnd
+    ret
+
+HelloString
+    dc "Hello world\n"
